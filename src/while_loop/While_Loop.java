95dab24985de49d49f5c5a59f86075be6ae5e6bd
joshmagic95 <99381454+joshmagic95@users.noreply.github.com>
2022-02-14 11:18:33 +0800

    Initial commit

---
+++ src/while_loop/While_Loop.java
@@ -0,0 +1,13 @@
+package while_loop;
+public class While_Loop {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i=0;
+        System.out.println("Printing the list of 10 even numbers");      
+        while (i<10){
+            System.out.println(i);
+            i=i+2;
+        }
+    }
+    
+}
